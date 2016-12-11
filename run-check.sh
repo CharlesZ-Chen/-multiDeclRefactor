4cb5dae3ddfa801723979bfd3c0e7f86da3a72b3
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-12-10 21:45:36 -0500

    fix path of multiDeclChecks.xml in running script

--- run-check.sh
+++ run-check.sh
@@ -4,7 +4,7 @@ ROOT_DIR=$(cd $SCRIPT_DIR/.. && pwd)
 
 CHECK_STYLE_ALL_JAR=$ROOT_DIR/checkstyle/target/checkstyle-7.4-SNAPSHOT-all.jar
 
-java -jar $CHECK_STYLE_ALL_JAR -c multiDeclChecks.xml -f json "$@"
+java -jar $CHECK_STYLE_ALL_JAR -c $SCRIPT_DIR/multiDeclChecks.xml -f json "$@"
 
 
 # mvn clean install -DskipTests -Dcheckstyle.ant.skip -Dcobertura.skip
--- run-refactor.sh
+++ run-refactor.sh
@@ -4,7 +4,7 @@ ROOT_DIR=$(cd $SCRIPT_DIR/.. && pwd)
 
 CHECK_STYLE_ALL_JAR=$ROOT_DIR/checkstyle/target/checkstyle-7.4-SNAPSHOT-all.jar
 
-java -jar $CHECK_STYLE_ALL_JAR -c multiDeclChecks.xml -f json "$@" |  python $SCRIPT_DIR/multiDeclRefactor.py
+java -jar $CHECK_STYLE_ALL_JAR -c $SCRIPT_DIR/multiDeclChecks.xml -f json "$@" |  python $SCRIPT_DIR/multiDeclRefactor.py
 
 ##develop version call: using eclipse output of checkstyle, running refactor frontend
 # java -cp $ROOT_DIR/checkstyle/target/classes:$(ls $ROOT_DIR/checkstyle/target/dependency/*.jar | tr '\n' : | rev | cut -c 2- | rev) com.puppycrawl.tools.checkstyle.Main -c $SCRIPT_DIR/multiDeclChecks.xml -f json "$@" | python $SCRIPT_DIR/multiDeclRefactor.py
